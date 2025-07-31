3c92988114fe57bd87015ec32c7d1c6cf8b3fb6c
scotty974 <fabienetheve@hotmail.com>
2025-07-31 15:53:34 +0200

    add get url

--- back-end/src/api/scrap/controller/scrapController.js
+++ back-end/src/api/scrap/controller/scrapController.js
@@ -1,25 +1,50 @@
 import generateResponse from "../utils/generateResponse.js";
-
+import * as fs from "fs";
 class ScrapController {
-        constructor( data ) {
-                this.data = data;
-        }
+  constructor(data) {
+    this.data = data;
+  }
+  async get_scrap_url(req, res) {
+    try {
+      this.data = req.body.url;
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "An error occurred while processing the request" });
+    }
+    const url_json = {
+      url: this.data,
+    };
 
-        async change_scrap_data(req, res){
-            try {
-                this.data = req.body;
-            }catch (error) {
-                return res.status(500).json({ error: "An error occurred while updating data" });
-            }
-            
-            const resp = await generateResponse(this.data.text);
-            if (!resp) {
-                return res.status(500).json({ error: "An error occurred while generating response" });
-            }
-            return res.status(200).json({ response: resp });
+    fs.writeFile('data/data.json', JSON.stringify(url_json), (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ error: "An error occurred while saving the URL" });
+      }
+      return res.status(200).json({
+        message: "URL saved successfully",
+      });
+    });
 
+  }
+  async change_scrap_data(req, res) {
+    try {
+      this.data = req.body;
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ error: "An error occurred while updating data" });
+    }
 
-        }
+    const resp = await generateResponse(this.data.text);
+    if (!resp) {
+      return res
+        .status(500)
+        .json({ error: "An error occurred while generating response" });
+    }
+    return res.status(200).json({ response: resp });
+  }
 }
 
 export default new ScrapController({});
