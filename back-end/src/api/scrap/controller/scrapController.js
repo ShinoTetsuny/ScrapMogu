2bf383ca1c7521ddd395a913289b62be4d53e690
scotty974 <fabienetheve@hotmail.com>
2025-07-31 14:13:16 +0200

    add api chatgpt

--- back-end/src/api/scrap/controller/scrapController.js
+++ back-end/src/api/scrap/controller/scrapController.js
@@ -1,5 +1,23 @@
+import generateResponse from "../utils/generateResponse";
+
 class ScrapController {
         constructor( data ) {
                 this.data = data;
+        }
+
+        async change_scrap_data(req, res){
+            try {
+                this.data = req.body;
+            }catch (error) {
+                return res.status(500).json({ error: "An error occurred while updating data" });
+            }
+            
+            const resp = await generateResponse(this.data.text);
+            if (!resp) {
+                return res.status(500).json({ error: "An error occurred while generating response" });
+            }
+            return res.status(200).json({ response: resp });
+
+
         }
 }
