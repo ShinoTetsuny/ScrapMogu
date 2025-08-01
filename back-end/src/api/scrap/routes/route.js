52a599efa3a5f03d538a6e2aa877f84538537270
scotty974 <fabienetheve@hotmail.com>
2025-08-01 16:37:26 +0200

    add routes with history

--- back-end/src/api/scrap/routes/route.js
+++ back-end/src/api/scrap/routes/route.js
@@ -4,4 +4,5 @@ import scrapController from "../controller/scrapController.js";
 
 
 router.post('/', (req, res) => { scrapController.get_scrap_url(req, res); });
+router.get('/history', (req, res) => { scrapController.get_history_scrap(req, res); });
 export default router;
